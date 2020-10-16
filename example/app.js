75932b8e5769737c62a607629dfb32a3e08ae9dd
Andrew <iAndrew3@protonmail.com>
2020-10-17 00:29:25 +0300

    Masonry - actions demo

--- example/app.js
+++ example/app.js
@@ -7,12 +7,13 @@ import Masonry from '../lib';
 const mockData = patternMock({
 	data: {
 		__pattern__: [{
-			id: 'COUNTER',
+			id: 'NUMBER',
 			title: 'FULL_NAME',
-			description: 'SENTENCE'
+			description: 'SENTENCE',
+			backgroundColor: 'COLOR'
 		}],
 		__config__: {
-			length: 7
+			length: 10
 		}
 	}
 }).data;
@@ -20,21 +21,34 @@ const mockData = patternMock({
 function App() {
 	const [data, setData] = useState(mockData);
 
-	const handleOnRemove = () => {}
+	const getNewItem = () => patternMock({
+		id: 'NUMBER',
+		title: 'FULL_NAME',
+		description: 'SENTENCE',
+		backgroundColor: 'COLOR'		
+	});
+
+	const handleOnRemoveAll = () => setData([]);
+	const handleOnRemove = item => setData(prevData => prevData.filter(({id}) => id !== item.id));
+	const handleOnAdd = () => setData(prevData => [...prevData, getNewItem()]);
 
 	return (
 		<View style={styles.wrapper}>
+		<View style={styles.actions}>
+			<Text onPress={handleOnAdd}>Add new item</Text>
+			<Text onPress={handleOnRemoveAll}>Remove all items</Text>
+		</View>
 			<Masonry
 				data={data}
 				numberOfColumns={4}
 				keyExtractor={item => item.id}
 				renderItem={({ item }) => {
-					const {title, description} = item;
+					const {title, description, backgroundColor} = item;
 
 					return (
 						<TouchableOpacity
 							onPress={() => handleOnRemove(item)}
-							style={styles.item}
+							style={[styles.item, {backgroundColor}]}
 						>
 							<Text style={styles.title}> {title} </Text>
 							<Text style={styles.description}> {description} </Text>
@@ -59,12 +73,21 @@ const styles = StyleSheet.create({
 		margin: 4,
 	},
 	title: {
+		color: '#fff',
 		fontSize: 14,
 		fontWeight: 'bold',
 		marginBottom: 4
 	},
 	description: {
+		color: '#fff',
 		fontSize: 12
+	},
+	actions: {
+		paddingVertical: 8,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		backgroundColor: '#fff',
+		marginBottom: 8
 	}
 });
 
